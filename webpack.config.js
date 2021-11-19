f8f3ff59b6d046fcf25776547c6e05e4a9aece04
MonkeyBinBin <thisisbinbin@gmail.com>
2021-11-20 01:00:01 +0800

    fixed css image url public path

--- webpack.config.js
+++ webpack.config.js
@@ -90,7 +90,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../../',
+              publicPath: '../',
               hmr: process.env.NODE_ENV === 'development'
             }
           },
